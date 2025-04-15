40e08005d68b9ac1c12492a57c148434f3a41000
carrotliang <liangkunyao@hotmail.com>
2025-04-15 10:17:27 +0800

    增加支持信息

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,4 +3,5 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 export const navbar = defineNavbarConfig([
   { text: '首页', link: '/' },
   { text: 'FAsset', link: '/fasset/' },
+  { text: '支持', link: '/support/' }
 ])
---
+++ docs/notes/support/contact.md
@@ -0,0 +1,16 @@
+---
+title: Support
+createTime: 2025/03/31 16:21:30
+permalink: /support/
+---
+
+## ContactUs
+
+
+### Email
+
+feedback@growcarrot.tech
+
+### Address
+
+北京，中国
